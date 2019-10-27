0945356d292eb54e26575072c48f46b4a62db131
akhdhys <akihide.94@gmail.com>
2019-10-27 15:23:37 +0900

    save

--- chmd/preprocess.py
+++ chmd/preprocess.py
@@ -1,3 +1,4 @@
+import io
 import numpy as np
 from chmd.neighbors import number_repeats, neighbor_duos
 from chainer.backend import get_array_module
@@ -25,6 +26,7 @@ def symbols_to_elements(symbols: np.ndarray,
     ret = np.full(symbols.shape, -1)
     for i, s in enumerate(order_of_symbols):
         ret = np.where(symbols == s, i, ret)
+    assert np.all(order_of_symbols[ret] == symbols)
     return ret
 
 
@@ -56,7 +58,32 @@ def preprocess_energy_forces(cell, pbc, positions, energy, forces, symbols, orde
             }
 
 
+def save(array):
+    """Save np.ndarray to byte."""
+    tmp = io.BytesIO()
+    np.save(tmp, array, allow_pickle=False)
+    return tmp.getvalue()
+
+
+def load(byte):
+    """Load bytes to np.ndarray."""
+    return np.load(io.BytesIO(byte), allow_pickle=False)
+
+
+def savetxt(array):
+    """Save np.ndarray to byte."""
+    tmp = io.StringIO()
+    np.savetxt(tmp, array)
+    return tmp.getvalue()
+
+
+def loadtxt(byte):
+    """Load bytes to np.ndarray."""
+    return np.loadtxt(io.StringIO(byte))
+
+
 def dict_to_hdf5(root, name, dic, attrs):
+    """Convert dict to hdf5 group."""
     grp = root.create_group(name)
     for key in attrs:
         grp.attrs[key] = attrs[key]
@@ -66,6 +93,7 @@ def dict_to_hdf5(root, name, dic, attrs):
 
 
 def hdf5_to_dict(hdf5):
+    """Read hdf5 group and convert to dict."""
     dic = dict()
     for key in hdf5:
         dic[key] = hdf5[key][()]
