6b5eb11178a37444a19c19035724694ff7e43914
akhdhys <akihide.94@gmail.com>
2019-11-11 20:34:29 +0900

    save

--- chmd/models/ani.py
+++ chmd/models/ani.py
@@ -1,7 +1,7 @@
 """ANI-1."""
 import numpy as np
 import chainer
-from chainer import Chain, Variable
+from chainer import Chain, Variable, ChainList, grad, report
 import chainer.functions as F
 from chmd.links.ani import ANI1AEV, ANI1AEV2Energy
 from chmd.links.shifter import EnergyShifter
@@ -14,30 +14,87 @@ def asarray(x):
         return x.data
     return x
 
+
 class ANI1(Chain):
     """ANI-1 energy calculator."""
 
-    def __init__(self, num_elements, aev_params, nn_params, cutoff, pbc):
+    def __init__(self, num_elements, aev_params, nn_params,
+                 cutoff, pbc, n_agents):
         """Initializer."""
         super().__init__()
         with self.init_scope():
             self.aev = ANI1AEV(num_elements, **aev_params)
-            self.energy = ANI1AEV2Energy(num_elements, nn_params)
+            self.energies = ChainList(*[ANI1AEV2Energy(num_elements, nn_params)
+                                        for _ in range(n_agents)])
             self.shift = EnergyShifter(num_elements)
         self.add_persistent('pbc', pbc)
         self.cutoff = cutoff
+        self.n_agents = n_agents
 
-    def forward(self, cells, elements, positions, affiliations,
-                adjacents1=None, adjacents2=None, shifts=None):
+    def forward(self, cells, elements, positions,
+                i1, i2=None, j2=None, s2=None):
         """Apply."""
-        if adjacents1 is None or adjacents2 is None or shifts is None:
+        if i2 is None or j2 is None or s2 is None:
             repeats = number_repeats(cells, self.pbc, self.cutoff)
             repeat = self.xp.max(repeats, axis=0)
-            (adjacents1, adjacents2, shifts) = neighbor_duos(
+            (i2, j2, s2) = neighbor_duos(
                 asarray(cells), asarray(positions),
-                self.cutoff, repeat, affiliations,)
-        aev = self.aev(cells, positions, elements, affiliations,
-                       adjacents1, adjacents2, shifts)
-        energy_nn = self.energy(aev, elements, affiliations, cells.shape[0])
-        energy_linear = self.shift(elements, affiliations)
-        return energy_nn + energy_linear
+                self.cutoff, repeat, i1,)
+        aev = self.aev(cells, positions, elements, i1, i2, j2, s2)
+        # parallel x batch
+        energy_nn = F.concat([en(aev, elements, i1, cells.shape[0])[None, :]
+                              for en in self.energies], axis=0)
+        assert energy_nn.shape[0] == self.n_agents
+        assert energy_nn.shape[1] == cells.shape[0]
+        assert energy_nn.ndim == 2
+        energy_linear = self.shift(elements, i1)
+        return energy_nn + energy_linear[None, :]
+
+
+class ANI1EnergyGradLoss(Chain):
+    """Energy + Grad."""
+
+    def __init__(self, predictor, ce, cf):
+        """Initializer.
+
+        Paramters
+        ---------
+        ce: coeffient for energy.
+        cf: coeffient for forces.
+
+        """
+        super().__init__()
+        with self.init_scope():
+            self.predictor = predictor
+        self.ce = ce
+        self.cf = cf
+
+    def __call__(self, positions, energies, forces, *args, **kwargs):
+        """Loss.
+
+        Parameters
+        ----------
+        target: Chain.
+        ri: positions.
+        e: Energy (ground truth.)
+        f: Force (ground truth.)
+
+        """
+        ri = Variable(positions)
+        # n_agents x n_batch
+        en = self.predictor(positions=ri, *args, **kwargs)
+        assert en.ndim == 2
+        loss_e = 0.0
+        loss_f = 0.0
+        for i in range(self.predictor.n_agents):
+            fi, = grad([-en[i, :]], [ri], enable_double_backprop=True)
+            assert ri.shape == fi.shape
+            loss_e += F.mean_squared_error(en[i], energies)
+            loss_f += F.mean_squared_error(fi, forces)
+        loss_e /= self.predictor.n_agents
+        loss_f /= self.predictor.n_agents
+        report({'loss_e': loss_e.data}, self)
+        report({'loss_f': loss_f.data}, self)
+        loss = self.ce * loss_e + self.cf * loss_f
+        report({'loss': loss.data}, self)
+        return loss
